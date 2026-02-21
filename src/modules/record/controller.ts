414432b96efa0825254742441c03025f1e72fef5
Hanif Ahmad <ahmadhanif759@gmail.com>
2026-02-21 14:46:28 +0700

    record list

--- src/modules/record/controller.ts
+++ src/modules/record/controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express"
 import RecordModel from "./model"
 import { sq } from "../../config/connection"
 import UserAccountModel from "../user_account/model"
+import { tipe } from "../../helpers/tipe"
 
 export class Controller {
   static async create(req: Request, res: Response, next: NextFunction) {
@@ -23,20 +24,94 @@ export class Controller {
     }
   }
   static async list(req: Request, res: Response, next: NextFunction) {
-    const { from_user_account_id, to_user_account_id, amount, type } = req.body
-    const t = await sq.transaction()
+    const {
+      id,
+      from_user_account_id,
+      to_user_account_id,
+      type,
+      date_action,
+      sub_category_code,
+      description,
+      order_by_name,
+      order_by_value,
+      current,
+      limit,
+    } = req.body
     try {
-      if (type == "expense") await UserAccountModel.decrement("amount", { by: amount, where: { id: from_user_account_id }, transaction: t })
-      else if (type == "income") await UserAccountModel.increment("amount", { by: amount, where: { id: from_user_account_id }, transaction: t })
-      else if (type == "transfer") {
-        await UserAccountModel.decrement("amount", { by: amount, where: { id: from_user_account_id }, transaction: t })
-        await UserAccountModel.increment("amount", { by: amount, where: { id: to_user_account_id }, transaction: t })
+      let value1 = ``
+      let value2 = ` order by r.date_action`
+
+      if (id) {
+        value1 += ` and r.id = :id`
       }
-      await RecordModel.create(req.body, { transaction: t })
-      await t.commit()
-      res.status(200).send({ status: 200, message: "success" })
+      if (from_user_account_id) {
+        value1 += ` and r.from_user_account_id = :from_user_account_id`
+      }
+      if (to_user_account_id) {
+        value1 += ` and r.to_user_account_id = :to_user_account_id`
+      }
+      if (type) {
+        value1 += ` and r.type = :type`
+      }
+      if (date_action) {
+        value1 += ` and r.date_action = :date_action`
+      }
+      if (sub_category_code) {
+        value1 += ` and r.sub_category_code = :sub_category_code`
+      }
+      if (description) {
+        value1 += ` and r.description ilike :description`
+      }
+      if (order_by_name && order_by_value) {
+        if (order_by_name == "date_action") {
+          value2 = ` order by r.date_action :order_by_value`
+        }
+      }
+      if (current && limit) {
+        value2 += ` offset :offset limit :limit`
+      }
+
+      const data = await sq.query(
+        `select r.*,ua.ms_account_code from_user_account_name,ua2.ms_account_code to_user_account_name,sc.sub_category_name from record r 
+        join user_account ua on ua.id = from_user_account_id 
+        left join user_account ua2 on ua2.id = to_user_account_id 
+        left join sub_category sc on sc.sub_category_code = r.sub_category_code
+        where r.id notnull${value1}${value2}`,
+        tipe({
+          id,
+          from_user_account_id,
+          to_user_account_id,
+          type,
+          date_action,
+          sub_category_code,
+          description: `%${description}%`,
+          order_by_value,
+          offset: +limit * (+current - 1),
+          limit,
+        })
+      )
+
+      if (current && limit) {
+        const count: any[] = await sq.query(
+          `select count(*)::int total from record r 
+          join user_account ua on ua.id = from_user_account_id 
+          where r.id notnull${value1}`,
+          tipe({
+            id,
+            from_user_account_id,
+            to_user_account_id,
+            type,
+            date_action,
+            sub_category_code,
+            description: `%${description}%`,
+            order_by_value,
+          })
+        )
+
+        return res.status(200).send({ status: 200, current: +current, total: count[0].total, message: "success", data })
+      }
+      res.status(200).send({ status: 200, message: "success", data })
     } catch (err) {
-      await t.rollback()
       next(err)
     }
   }
--- src/modules/user_account/controller.ts
+++ src/modules/user_account/controller.ts
@@ -4,6 +4,7 @@ import { sq } from "../../config/connection"
 import { tipe } from "../../helpers/tipe"
 import { IJwtPayload } from "../../helpers/jsonwebtoken"
 import RecordModel from "../record/model"
+import { Op } from "sequelize"
 
 export class Controller {
   static async bulkCreate(req: Request, res: Response, next: NextFunction) {
@@ -22,7 +23,7 @@ export class Controller {
     const t = await sq.transaction()
     try {
       const { id } = req.params
-      await RecordModel.destroy({ where: { user_account_id: id }, transaction: t })
+      await RecordModel.destroy({ where: { [Op.or]: { from_user_account_id: id, to_user_account_id: id } }, transaction: t })
       await UserAccountModel.destroy({ where: { id }, transaction: t })
       await t.commit()
       res.status(200).send({ status: 200, message: "success" })
@@ -33,7 +34,7 @@ export class Controller {
   }
   static async list(req: Request, res: Response, next: NextFunction) {
     try {
-      const { page, limit, id, ms_account_code } = req.body
+      const { current, limit, id, ms_account_code } = req.body
       const { id: ms_user_id } = req?.user as IJwtPayload
 
       let filter = ``
@@ -45,7 +46,7 @@ export class Controller {
       if (ms_account_code) {
         filter += ` and ua.ms_account_code = :ms_account_code`
       }
-      if (page && limit) {
+      if (current && limit) {
         filter2 += ` offset :offset limit :limit`
       }
 
@@ -53,18 +54,17 @@ export class Controller {
         `select ua.id,ua.ms_account_code,msa.ms_account_name,ua.amount::int from user_account ua
         join ms_account msa on msa.ms_account_code = ua.ms_account_code
         where ua.ms_user_id = :ms_user_id and ua.deleted_at isnull${filter}${filter2}`,
-        tipe({ offset: (page - 1) * limit, limit, id, ms_user_id, ms_account_code })
+        tipe({ offset: (current - 1) * limit, limit, id, ms_user_id, ms_account_code })
       )
 
-      if (page && limit) {
-        const total: any[] = await sq.query(
-          `select count(*)::int as count from user_account ua
+      if (current && limit) {
+        const count: any[] = await sq.query(
+          `select count(*)::int as total from user_account ua
           join ms_account msa on msa.ms_account_code = ua.ms_account_code
           where ua.ms_user_id = :ms_user_id and ua.deleted_at isnull${filter}`,
           tipe({ id, ms_user_id, ms_account_code })
         )
-        res.status(200).send({ status: 200, page: +page, total: total[0].count, message: "success", data })
-        return
+        return res.status(200).send({ status: 200, current: +current, total: count[0].total, message: "success", data })
       }
 
       res.status(200).send({ status: 200, message: "success", data })
